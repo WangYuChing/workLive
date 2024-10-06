aeff8dfaa22ac8f179685b68a771264f1a832b00
昱晴 王 <5b1g0023@stust.edu.tw>
2024-10-06 10:07:46 +0800

    Learn js

--- home.js
+++ home.js
@@ -1,3 +1,11 @@
+/*  const startScreen 宣告一個不可變的常量變數startScreen
+    = 將等號右側表達式的結果送給左邊的變數StartScreen
+    document 全域物件，表示目前的HTML文件
+    .getElementById() 物件提供的方法之一，從元素(Element)的id屬性來尋找HTML中的特定元素
+    'strat-Screen 字串，HTML文件中某個元素的id值
+透過document.getElementById()找到HTML文件中id為start-Screen的元素，
+並將此元素的內容送給變數startScreen */
+
 const startScreen = document.getElementById('start-Screen');
 const startButton = document.getElementById('start-Button');
 const gameBox = document.getElementById('game-Box');
@@ -5,7 +13,7 @@ const dialogueBox = document.getElementById('dialogue-Box');
 const dialogueText = document.getElementById('dialogue-Text');
 const optionBox = document.getElementById('option-Box');
 
-startButton.addEventListener('click', startGame);
+startButton.addEventListener('click', startGame);//當按鈕被點擊，開始遊戲
 function startGame(){
     startScreen.classList.add('fade-out-StartScreen');
     startButton.classList.add('fade-out-StartButton');
