5a3463606aa11d676e8c98019887bf69e2b55373
昱晴 王 <5b1g0023@stust.edu.tw>
2024-10-06 12:09:19 +0800

    js 1.定義一個名為updateDialogue的function:更新當前對話內容 2.將選項逐一加進選項容器optionBox

--- home.js
+++ home.js
@@ -11,11 +11,12 @@ const startButton = document.getElementById('start-Button');//開始按鈕
 const gameBox = document.getElementById('game-Box');//遊戲畫面
 const dialogueBox = document.getElementById('dialogue-Box');//劇情框
 const dialogueText = document.getElementById('dialogue-Text');//劇情文字
-const optionBox = document.getElementById('option-Box');//選項
+const optionBox = document.getElementById('option-Box');//選項容器
 
 //當按鈕被點擊，呼叫名為startGame的function
 startButton.addEventListener('click', startGame);
-function startGame(){//定義一個名為startGame的function
+//定義一個名為startGame的function
+function startGame(){
     startScreen.classList.add('fade-out-StartScreen');//classList DOM元素的屬性，表示該元素擁有的所有CSS類別（class）。一個包含這些類別名稱的物件，提供了許多可以操作類別的方法
     startButton.classList.add('fade-out-StartButton');//add() classList物件上的方法，向DOM元素添加()中類別的樣式。class 或 id。
     setTimeout(() => { // js的內建函式，作用:延遲執行。在指定的延遲時間（以毫秒計）後執行一段程式碼或函數。
@@ -28,3 +29,18 @@ function startGame(){//定義一個名為startGame的function
 // 當前的對話階段。初始值為 0
 let currentDialogue = 0;
 
+//定義一個名為updateDialogue的function:更新當前對話內容
+function updateDialogue(){
+    const dialogue = dialogue[currentDialogue];//取得當前對話
+    dialogueText.textContent = dialogue.text;//更新劇情文字
+    optionBox.innerHTML = "";//清空選項容器
+
+    //將選項逐一加進選項容器optionBox
+    dialogue.option.forEach(option => {
+        const optionElement = document.createElement('div');// 建立選項元素
+        optionElement.classList.add('option');//新增class類別樣式.option
+        optionElement.textContent = option.text;//設定選項文字
+        optionElement.setAttribute('dataChoice',option.choice);// 為變數optionElement設置一個自定義屬性dataChoice，值來自option.choice
+        optionBox.appendChild(optionElement);//appendChild 將新的子元素(optionElement)加入指定父元素(optionBox)
+    });
+}
